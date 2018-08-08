92c6d33699afe1a745f71499b2f421dee3304537
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-08-08 17:37:53 -0500

    complete tutorial

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -43,19 +43,19 @@ Instructions:
         req.onload = function() {
           if (req.status === 200) {
             resolve(req.response);
-            addSearchHeader();
+  
             // It worked!
             // You'll want to resolve with the data from req.response
           } else {
             // It failed :(
             // Be nice and reject with req.statusText
-            reject(req.statusText);
+            reject(Error(req.statusText));
           }
         };
         req.onerror = function() {
           // It failed :(
           // Pass a 'Network Error' to reject
-          reject(error);
+          reject(Error('Network Error'));
           // addSearchHeader('unknown');
         };
         req.send();
@@ -70,6 +70,14 @@ Instructions:
       pass 'unknown' to addSearchHeader if it rejects.
        */
       get('../data/earth-like-results.json')
+        .then(function(response) {
+          addSearchHeader(response);
+        })
+        .catch(function(error) {
+          addSearchHeader('unknown');
+          console.log(error);
+        })
+
     });
 
 })(document);
